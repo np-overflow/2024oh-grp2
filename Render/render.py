e687cd5061aaba3119435a07e2ca4fa857d623f0
gnayuy <87360806+gnay-uy@users.noreply.github.com>
2023-11-30 21:48:37 +0800

    start work on render

---
+++ Render/render.py
@@ -0,0 +1,13 @@
+import cv2
+from ascii_magic import AsciiArt
+
+def render_image(path):
+    frame = AsciiArt.from_image(path)
+
+    frame.to_terminal(monochrome=True)
+
+def size_check(path):
+    img = cv2.imread(path)
+    w, h, _ = img.shape
+
+    return w == h == 500
