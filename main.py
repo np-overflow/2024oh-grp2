a08f8d5ea39182bc859e1fcd8bace2023898bf99
whywhy <87360806+gnay-uy@users.noreply.github.com>
2023-12-01 12:47:07 +0800

    add commandline stuff to main

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
-import requests
-from Render import render
+# imports
+import sys
+import argparse
 
-# program entry point
+def banner():
+    return """
+    Overflow Open House Project!
 
-def __main__():
-    URL_TO_IMAGE = input("Enter URL to image: ")
-    image = requests.get(URL_TO_IMAGE).content
+    Credits:
+        Yu Yang
+        Jayden
+        Alfred
+        Brayden
+    """
 
-    # TO IMPLEMENT : resize image
+def parse_args():
+    parser = argparse.ArgumentParser(description="Overflow Open House Project")
+
+    parser.add_argument(
+        "-u",
+        "--url",
+        help="takes a url argument containing the image"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--path",
+        help="specifies a path for the program to get image from"
+    )
+
+    parser.add_argument(
+        "-m",
+        "--mode",
+        help="specifies what effects to apply to the input image, or defaults to boioioing"
+    )
+
+    if len(sys.argv) == 1:
+        print(banner())
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
+
+def main(): # program main entry point
+    args = parse_args()
+
+
+if __name__ == "__main__":
+    main()
