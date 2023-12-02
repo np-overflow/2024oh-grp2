14f757eb54515526d8bd10a5c53dbe4e91ece54c
gnayuy <87360806+gnay-uy@users.noreply.github.com>
2023-12-03 03:56:54 +0800

    implement image downloading and resizing

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # imports
+import Render.render as render
+import Transform.transform as transform
+
+import os
 import sys
 import argparse
+import requests
+import imghdr
+
+from uuid import uuid4 as uuid
+
+# modes - add as needed
+MODE_BOING = "boioioing"
+MODE_DEBUG = "debug"
+MODE_TEST = "test"
 
 def banner():
     return """
@@ -9,9 +22,7 @@ def banner():
    ___   __   __   ___   _ __  | |_  | |   ___   __      __
   / _ \  \ \ / /  / _ \ | '__| |  _| | |  / _ \  \ \ /\ / /
  | (_) |  \ V /  |  __/ | |    | |   | | | (_) |  \ V  V / 
-  \___/    \_/    \___| |_|    |_|   |_|  \___/    \_/\_/  
-                                                           
-                                                           
+  \___/    \_/    \___| |_|    |_|   |_|  \___/    \_/\_/                                                 
 
 Credits:
     Yu Yang
@@ -21,7 +32,7 @@ Credits:
     """
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="boioioioing")
+    parser = argparse.ArgumentParser(description="boioioing")
 
     parser.add_argument(
         "-u",
@@ -38,7 +49,10 @@ def parse_args():
     parser.add_argument(
         "-m",
         "--mode",
-        help="specifies what effects to apply to the input image, or defaults to boioioing"
+        help="specifies what effects to apply to the input image, defaults to boioioing",
+        default="boioioing",
+        choices=[MODE_BOING, MODE_DEBUG, MODE_TEST],
+        nargs="?"
     )
 
     if len(sys.argv) == 1:
@@ -49,8 +63,32 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def get_image(url, save_path):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open(save_path, 'wb') as file_handle: # 'wb' - write mode for binary files
+            file_handle.write(response.content)
+        
+        image_format = imghdr.what(save_path)
+        new_path = f"{save_path}.{image_format}"
+        os.rename(save_path, new_path)
+
+    else:
+        raise Exception("Cannot get image")
+
+    return new_path
+
 def main(): # program main entry point
     args = parse_args()
+    mode = args.mode
+    save_path = "./temp/"
+    rng = str(uuid())
+
+    if args.path is not None:
+        image_src = transform.resize_image(args.path)
+    else:
+        image_src = transform.resize_image(get_image(args.url, save_path+rng))
 
 
 if __name__ == "__main__":
