05ac37faa7c95bb1960f869b775759adbc76b726
gnayuy <87360806+gnay-uy@users.noreply.github.com>
2023-12-03 02:35:36 +0800

    fix error msg error and filename issue in unittest for render

--- Render/unittests.py
+++ Render/unittests.py
@@ -4,18 +4,18 @@ import render
 class TestRenderMethods(unittest.TestCase):
 
     def testBadImages(self):
-        res = render.size_check("Render/SizeTest/1000x1020.png")
-        self.assertFalse(res, "500x520 image should not return True")
+        res = render.size_check("Render/SizeTest/1000x1200.png")
+        self.assertFalse(res, "1000x1020 image should not return True")
 
         res = render.size_check("Render/SizeTest/1200x1000.png")
-        self.assertFalse(res, "520x500 image should not return True")
+        self.assertFalse(res, "1200x1000 image should not return True")
 
         res = render.size_check("Render/SizeTest/1200x1200.png")
-        self.assertFalse(res, "520x520 image should not return True")
+        self.assertFalse(res, "1200x1200 image should not return True")
 
     def testGoodImage(self):
         res = render.size_check("Render/SizeTest/1000x1000.png")
-        self.assertTrue(res, "500x500 image should not return False")
+        self.assertTrue(res, "1000x1000 image should not return False")
 
 
 if __name__ == "__main__":
