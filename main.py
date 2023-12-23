6d336cda74c1ffe8cdbae0cad02addcc33f5428d
gnay-uy <87360806+gnay-uy@users.noreply.github.com>
2023-12-23 11:15:07 +0800

    added image generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # imports
 import Render.render as render
+import Render.imagen as imagen
 import Transform.transform as transform
 
 import os
@@ -12,26 +13,12 @@ import requests
 
 from uuid import uuid4 as uuid
 
-# modes - add as needed
-MODE_BOING = "boioioing"
-MODE_DEBUG = "debug"
-MODE_TEST = "test"
 
 def banner():
     return """
-                                 __   _                    
-                                / _| | |                   
-   ___   __   __   ___   _ __  | |_  | |   ___   __      __
-  / _ \  \ \ / /  / _ \ | '__| |  _| | |  / _ \  \ \ /\ / /
- | (_) |  \ V /  |  __/ | |    | |   | | | (_) |  \ V  V / 
-  \___/    \_/    \___| |_|    |_|   |_|  \___/    \_/\_/                                                 
-
-Credits:
-    Yu Yang
-    Jayden
-    Alfred
-    Brayden
+    credit: yy
     """
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="boioioing")
@@ -53,7 +40,14 @@ def parse_args():
         "--mode",
         help="specifies what effects to apply to the input image, defaults to boioioing",
         default="boioioing",
-        choices=[MODE_BOING, MODE_DEBUG, MODE_TEST],
+        choices=["boioioing", "test", "debug"],
+        nargs="?"
+    )
+
+    parser.add_argument(
+        "--generate",
+        help="generate an image with stable diffusion",
+        default="NO_GEN",
         nargs="?"
     )
 
@@ -69,6 +63,7 @@ def parse_args():
 
     args = parser.parse_args()
     return args
+
 
 def get_image(url, save_path):
     response = requests.get(url)
@@ -86,6 +81,7 @@ def get_image(url, save_path):
 
     return new_path
 
+
 def main(): # program main entry point
     save_path = "./temp/"
 
@@ -97,6 +93,8 @@ def main(): # program main entry point
     rng = str(uuid())
 
     os.mkdir(save_path)
+    if args.generate != "NO_GEN":
+        args.path = imagen.generate(args.generate, save_path+rng)
 
     if args.path is not None:
         image_src = transform.resize_image(args.path)
